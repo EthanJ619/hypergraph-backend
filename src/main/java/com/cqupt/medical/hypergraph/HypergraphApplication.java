1aae875f8434138334209e41f4e5737504cae84f
EthanJ <ethanjean@163.com>
2023-06-20 23:45:42 +0800

    firstVer

---
+++ src/main/java/com/cqupt/medical/hypergraph/HypergraphApplication.java
@@ -0,0 +1,13 @@
+package com.cqupt.medical.hypergraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypergraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HypergraphApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cqupt/medical/hypergraph/HypergraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cqupt.medical.hypergraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypergraphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
